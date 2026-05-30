d85611155d41fcfe0ca2d36b01b8c146a847da11
farfar184 <farmid1845@gmail.com>
2026-05-30 19:35:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -227,4 +227,4 @@ function updateStats() {
 loadTasksFromCloud();
 
 // Otomatis cek pembaruan data dari cloud setiap 10 detik sekali (Biar sinkron otomatis)
-setInterval(loadTasksFromCloud, 30000);
+setInterval(loadTasksFromCloud, 3000);
